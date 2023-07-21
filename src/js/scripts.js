8ac7c126b94b4def549c6d03aa9b22abab500399
James Grieve <jamesonrgrieve@gmail.com>
2023-07-20 19:20:21 -0600

    Quotations;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,14 +1,14 @@
 async function main() {
-    // This is where the code you're actually experimenting with goes.
-    
-    let prompt = "Please enter your name, or 'Exit' to quit: "
-    let name = await input(prompt);
-
-    while (name != "Exit") 
-    {
-        output("Hello, "+name+"!");
-        name = await input(prompt);
-    }
+    // If the string contains apostrophes, you can use double quotes to enclose it.
+    output("Jimmy's mom went to the store.");
+    // Otherwise you can escape them.
+    output('Jimmy\'s mom went to the store.');
+    // If the string contains double quotes, you can use single quotes to enclose it.
+    output('Jimmy says, "Hello!"');
+    // Otherwise you can escape them.
+    output("Jimmy says, \"Hello!\"");
+    // If the string contains both, you can use backticks to enclose it.
+    output(`Jimmy's mom says, "Jimmy says, 'Hello!'"`);
 }
 
 
