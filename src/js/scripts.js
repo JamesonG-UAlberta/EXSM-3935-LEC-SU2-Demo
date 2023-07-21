204b4d0956925979081e3cec64dc728e50340006
James Grieve <jamesonrgrieve@gmail.com>
2023-07-20 19:16:20 -0600

    Concise solution;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,13 +1,6 @@
 async function main() {
-    let firstName = await input("What is your first name?: ");
-    let lastName = await input("What is your last name?: ");
-    const fullName = `${firstName} ${lastName}`.replaceAll('o', 'a');
-    output(fullName);
-
-    let bigNumber = await input("Enter a big number (larger than 10): ");
-    let smallNumber = await input("Enter a small number (smaller than 10): ");
-    let result = bigNumber % smallNumber;
-    output(`${bigNumber} % ${smallNumber} = ${result}`);
+    output(`${await input("What is your first name?: ")} ${await input("What is your last name?: ")}`.replaceAll('o', 'a'));
+    output(`The result is ${await input("Enter a big number (larger than 10): ") % await input("Enter a small number (smaller than 10): ")}.`);
 }
 
 
