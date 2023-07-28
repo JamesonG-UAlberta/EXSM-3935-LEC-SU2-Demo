58abdf9ed506fbb6138eb6b9d0a5cd716d413be2
James Grieve <jamesonrgrieve@gmail.com>
2023-07-27 19:23:31 -0600

    Solution;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,14 +1,46 @@
 async function main() {
-    // This is where the code you're actually experimenting with goes.
-    
-    let prompt = "Please enter your name, or 'Exit' to quit: "
-    let name = await input(prompt);
+    let costOfGoods = await input("Please enter the total cost of goods (omit dollar sign): ");
+    let cashPayment = await input("Please enter the payment rendered by the customer (omit dollar sign): ");
+    let discountCode = await input("Please enter the discount code (if present, hit enter to skip): ");
 
-    while (name != "Exit") 
+    let discountPercentage;
+    switch (discountCode) 
     {
-        output("Hello, "+name+"!");
-        name = await input(prompt);
+        case "0": 
+            discountPercentage = 0;
+            break;
+        case "1": 
+            discountPercentage = 0.1;
+            break;
+        case "2": 
+            discountPercentage = 0.15;
+            break;
+        case "3": 
+            discountPercentage = 0.25;
+            break;
+        case "4": 
+            discountPercentage = 0.35;
+            break;
+        case "5": 
+            discountPercentage = 0.40;
+            break;
+        default: 
+            discountPercentage = 0;
+            output("Invalid discount code entered. No discount applied.");
+            break;
     }
+    const discountedCost = costOfGoods*(1-discountPercentage);
+    const change = cashPayment-(discountedCost);
+    if (change < 0)
+    {
+        output("Insufficient payment rendered. Sorry, you cannot afford the goods.");
+    }
+    else
+    {
+        output(`Your total after discounts is ${discountedCost}, you provided ${cashPayment}. Your change is ${change}.`);
+    }
+    
+
 }
 
 
