6aa5768c2d9a9570b7e2ab2b13043c5bd4dc7424
James Grieve <jamesonrgrieve@gmail.com>
2023-08-03 19:24:20 -0600

    Solution;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,7 @@
 async function main() {
 
+    const orderNumbers = new Set(["ABC101", "DEF202"]);
+    const quantities = [5, 12];
     try
     {
         let orderNumber = await input("Please enter your 6 character order number: ");
@@ -7,7 +9,21 @@ async function main() {
         {
             throw new Error("Your order number is invalid. It must be 6 alphanumeric characters.");
         }
-        output(orderNumber);
+        if (orderNumbers.has(orderNumber.toUpperCase()))
+        {
+            throw new Error("Your order number already exists.");
+        }
+        orderNumbers.add(orderNumber);
+        let quantity = Number(await input("Please enter the quantity: "));
+        if (isNaN(quantity))
+        {
+            throw new Error("Your quantity was not valid. It must be a number between 1 and 100 inclusive.");
+        }
+        if (quantity < 1 || quantity > 100)
+        {
+            throw new Error ("The quantity must be between 1 and 100 inclusive.");
+        }
+        quantities.push(quantity);
     }
     catch (exception)
     {
