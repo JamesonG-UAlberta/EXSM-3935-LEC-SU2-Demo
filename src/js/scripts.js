7d6a934158baf9c3c3b62c45d7dcc5a92391b25f
James Grieve <jamesonrgrieve@gmail.com>
2023-08-10 19:48:06 -0600

    Example 2 (no assignment);

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,14 +3,12 @@ async function main() {
     output(myArray);
     // Arrays (and objects) are passed to arguments by REFERENCE (meaning they tether to the original copy from the caller.)
     // Any modifications made within the function WILL APPLY TO THE ORIGINAL.
-    const modifiedArray = await addItemToArray(myArray);
+    await addItemToArray(myArray);
     output(myArray);
-    output(modifiedArray);
 
 }
 // async is necessary if we are using input.
 async function addItemToArray(theArray) {
     const userInput = await input("Please enter the item for the array: ");
     theArray.push(userInput);
-    return theArray;
 }
