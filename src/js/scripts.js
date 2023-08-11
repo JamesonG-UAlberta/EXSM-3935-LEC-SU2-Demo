c6205aa25d3f63914157da3a0dd3e61c8fc63ca6
James Grieve <jamesonrgrieve@gmail.com>
2023-08-10 20:21:29 -0600

    Solution;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,14 +1,32 @@
 async function main() {
-    // This is where the code you're actually experimenting with goes.
-    
-    let prompt = "Please enter your name, or 'Exit' to quit: "
-    let name = await input(prompt);
-
-    while (name != "Exit") 
+    greeter(await getName(), await piggyBank());
+}
+async function getName() {
+    let name = (await input("Please enter your name: ")).trim();
+    if (name == "")
     {
-        output("Hello, "+name+"!");
-        name = await input(prompt);
+        name = "child1";
+    }
+    return name;
+}
+async function piggyBank() {
+    const inputString = (await input("Please enter your deposits in CSV (comma-separated value) format: ")).trim();
+    const deposits = inputString.split(",");
+    // https://stackoverflow.com/questions/1230233/how-to-find-the-sum-of-an-array-of-numbers
+    const sum = deposits.reduce((partialSum, a) => partialSum + Number(a), 0);
+    return sum;
+}
+function greeter(name, savings) {
+    if (savings < 10)
+    {
+        output(`Hello ${name}! You’ve stored ${savings} so far. Better keep saving!`);
+    }
+    else if (savings <= 100)
+    {
+        output(`Hello ${name}! You’ve stored ${savings} so far. Good job!`);
+    }
+    else
+    {
+        output(`Hello ${name}! You’ve stored ${savings} so far. You should really spend some of it!`);
     }
 }
-
-
