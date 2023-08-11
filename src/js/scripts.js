2a20bd5635f39697c95bbcca7b42be0f0c93cdc6
James Grieve <jamesonrgrieve@gmail.com>
2023-08-10 19:46:02 -0600

    Example 1 (with assignment);

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,51 +1,16 @@
 async function main() {
-    try {
-        // Await is necessary for our functions with inputs to avoid concurrent inputs.
-        const operator = await getValidInput("Please enter a operator: ");
-        const left = await getValidInput("Please enter a left operand: ");
-        const right = await getValidInput("Please enter a right operand: ");    
-        output(`The result is ${calculator(operator, left, right)}.`);
-    }
-    catch (exception) {
-        output("ERROR: "+exception);
-    }
-    
-}
-// Inputs and Outputs
-function calculator(operator, leftOperand, rightOperand) {
-    let result;
-    // Parameters, at the end of the day, behave like local variables. So we can reassign / format them as needed.
-    operator = operator.trim();
-    leftOperand = Number(leftOperand);
-    rightOperand = Number(rightOperand);
-    if (operator == "+")
-    {
-        result = leftOperand + rightOperand;
-    }
-    else if (operator == "-")
-    {
-        result = operator - rightOperand;
-    }
-    else if (operator == "*")
-    {
-        result = leftOperand * rightOperand;
-    }
-    else if (operator == "/")
-    {
-        result = leftOperand / rightOperand;
-    }
-    else 
-    {
-        result = NaN;
-    }
-    return result;
+    const myArray = ["Hello", "World"];    
+    output(myArray);
+    // Arrays (and objects) are passed to arguments by REFERENCE (meaning they tether to the original copy from the caller.)
+    // Any modifications made within the function WILL APPLY TO THE ORIGINAL.
+    const modifiedArray = await addItemToArray(myArray);
+    output(myArray);
+    output(modifiedArray);
+
 }
 // async is necessary if we are using input.
-async function getValidInput(prompt) {
-    const userInput = await input(prompt);
-    if (userInput.trim() == "")
-    {
-        throw new Error("You did not enter a value.");
-    }
-    return userInput;
+async function addItemToArray(theArray) {
+    const userInput = await input("Please enter the item for the array: ");
+    theArray.push(userInput);
+    return theArray;
 }
