625d143f817b231f073a297ead343e6a351ea83a
James Grieve <jamesonrgrieve@gmail.com>
2023-08-14 19:46:14 -0600

    Demo for of;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,16 +1,19 @@
 async function main() {
-    for (/* Initialize */ let quantity = 1; /* Test */ quantity <= 10; /* Modify */ quantity++)
-    {
-        // Process
-        output(quantity);
-    }
-
     // Iterating over an array.
     const outputs = ["Hello", "World", "123", "Testing"];
     for (let outputIndex = 0; outputIndex < outputs.length; outputIndex++)
     {
         output(outputs[outputIndex]);
     }
+
+    // In this situation we do NOT have access to the index directly - only the item at that index.
+    // So if you don't need the index, this is a shorthand way to iterate over a loop.
+    // Keep in mind that mutating (changing) the value through this iterator variable is frowned upon in most cases,
+    // and can even throw exceptions in some languages. For modifying the array, a traditional for loop is usually used.
+    for (let thisOutput of outputs)
+    {
+        output(thisOutput);
+    }    
 }
 
 
