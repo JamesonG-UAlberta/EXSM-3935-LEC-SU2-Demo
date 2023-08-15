c943a548ecf048b41f670b8a7240e2d43948986f
James Grieve <jamesonrgrieve@gmail.com>
2023-08-14 19:39:54 -0600

    Simple iteration loop;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,30 +1,9 @@
 async function main() {
-    // This is where the code you're actually experimenting with goes.
-    
-    let prompt = "Please enter your name, or 'Exit' to quit: "
-    
-    // Initialize
-    //let name = await input(prompt);
-
-    // Test
-    // While loops are just like decisions - you can replace the word "while" with "if" and it will function just fine.
-    // The caveat is that "while" will run again as soon as it's done (including the test).
-    //while (name != "Exit") 
-    //{
-        
-        // Modify
-        // You MUST modify at least one variable used in your condition to avoid infinite loops (somewhere in the loop).
-        //name = await input(prompt);
-    //}
-
-
-    for (let name = await input(prompt); name != "Exit"; name = await input(prompt))
+    for (/* Initialize */ let quantity = 1; /* Test */ quantity <= 10; /* Modify */ quantity++)
     {
         // Process
-        output("Hello, "+name+"!");
+        output(quantity);
     }
-
-    
 }
 
 
