c0d27334c4fe21c5b2269bcae0553252502293a1
James Grieve <jamesonrgrieve@gmail.com>
2023-08-14 20:42:29 -0600

    Using a dictionary;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -24,12 +24,17 @@ async function main() {
 async function playGame() {
     const player1Token = "X";
     const player2Token = "O";
-    const gameBoard = [
-        ["1","2","3"],
-        ["4","5","6"],
-        ["7","8","9"]
-    ];
-    const availableSpaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    const gameBoard = {
+        "1":"1",
+        "2":"2",
+        "3":"3",
+        "4":"4",
+        "5":"5",
+        "6":"6",
+        "7":"7",
+        "8":"8",
+        "9":"9"
+    }
     let playerTurn = player1Token;
     let turnCount = 1;
     while (!checkWin(gameBoard) && turnCount <= 9)
@@ -38,48 +43,12 @@ async function playGame() {
         let playerSpace;
         do {
             playerSpace = (await input (`Player ${playerTurn}, please enter the space you would to play: `)).trim();
-            if (!availableSpaces.includes(playerSpace))
+            if (gameBoard[playerSpace] != playerSpace)
             {
                 output("Invalid input, please make another selection.");
             }
-        } while (!availableSpaces.includes(playerSpace));
-        availableSpaces.splice(availableSpaces.indexOf(playerSpace), 1);
-        if (playerSpace == "1" && gameBoard[0][0] == "1")
-        {
-            gameBoard[0][0] = playerTurn;
-        }
-        else if (playerSpace == "2" && gameBoard[0][1] == "2")
-        {
-            gameBoard[0][1] = playerTurn;
-        }
-        else if (playerSpace == "3" && gameBoard[0][2] == "3")
-        {
-            gameBoard[0][2] = playerTurn;
-        }
-        else if (playerSpace == "4" && gameBoard[1][0] == "4")
-        {
-            gameBoard[1][0] = playerTurn;
-        }
-        else if (playerSpace == "5" && gameBoard[1][1] == "5")
-        {
-            gameBoard[1][1] = playerTurn;
-        }
-        else if (playerSpace == "6" && gameBoard[1][2] == "6")
-        {
-            gameBoard[1][2] = playerTurn;
-        }
-        else if (playerSpace == "7" && gameBoard[2][0] == "7")
-        {
-            gameBoard[2][0] = playerTurn;
-        }
-        else if (playerSpace == "8" && gameBoard[2][1] == "8")
-        {
-            gameBoard[2][1] = playerTurn;
-        }
-        else if (playerSpace == "9" && gameBoard[2][2] == "9")
-        {
-            gameBoard[2][2] = playerTurn;
-        }
+        } while (gameBoard[playerSpace] != playerSpace);
+        gameBoard[playerSpace] = playerTurn;
         playerTurn = playerTurn == player1Token ? player2Token : player1Token;
         turnCount++;
     }
@@ -98,21 +67,21 @@ function checkWin(gameBoard) {
     let state;
     if (
         // Top Horizontal Line
-        (gameBoard[0][0] == gameBoard[0][1] && gameBoard[0][0] == gameBoard[0][2]) || 
+        (gameBoard["1"] == gameBoard["2"] && gameBoard["1"] == gameBoard["3"]) || 
         // Middle Horizontal Line
-        (gameBoard[1][0] == gameBoard[1][1] && gameBoard[1][0] == gameBoard[1][2]) || 
+        (gameBoard["4"] == gameBoard["5"] && gameBoard["4"] == gameBoard["6"]) || 
         // Bottom Horizontal Line
-        (gameBoard[2][0] == gameBoard[2][1] && gameBoard[2][0] == gameBoard[2][2]) || 
+        (gameBoard["7"] == gameBoard["8"] && gameBoard["7"] == gameBoard["9"]) || 
         // Left Vertical Line
-        (gameBoard[0][0] == gameBoard[1][0] && gameBoard[0][0] == gameBoard[2][0]) || 
+        (gameBoard["1"] == gameBoard["4"] && gameBoard["1"] == gameBoard["7"]) || 
         // Middle Vertical Line
-        (gameBoard[0][1] == gameBoard[1][1] && gameBoard[0][1] == gameBoard[2][1]) || 
+        (gameBoard["2"] == gameBoard["5"] && gameBoard["2"] == gameBoard["8"]) || 
         // Right Vertical Line
-        (gameBoard[0][2] == gameBoard[1][2] && gameBoard[0][2] == gameBoard[2][2]) || 
+        (gameBoard["3"] == gameBoard["6"] && gameBoard["3"] == gameBoard["9"]) || 
         // NW-SE Diagonal
-        (gameBoard[0][0] == gameBoard[1][1] && gameBoard[0][0] == gameBoard[2][2]) || 
+        (gameBoard["1"] == gameBoard["5"] && gameBoard["1"] == gameBoard["9"]) || 
         // NE-SW Diagonal
-        (gameBoard[0][2] == gameBoard[1][1] && gameBoard[0][2] == gameBoard[2][0])
+        (gameBoard["3"] == gameBoard["5"] && gameBoard["3"] == gameBoard["7"])
     )
     {
         state = true;
@@ -125,10 +94,10 @@ function checkWin(gameBoard) {
 }
 function displayBoard(gameBoard) {
     output("-------------");
-    output(`| ${gameBoard[0][0]} | ${gameBoard[0][1]} | ${gameBoard[0][2]} |`);
+    output(`| ${gameBoard["1"]} | ${gameBoard["2"]} | ${gameBoard["3"]} |`);
     output("-------------");
-    output(`| ${gameBoard[1][0]} | ${gameBoard[1][1]} | ${gameBoard[1][2]} |`);
+    output(`| ${gameBoard["4"]} | ${gameBoard["5"]} | ${gameBoard["6"]} |`);
     output("-------------");
-    output(`| ${gameBoard[2][0]} | ${gameBoard[2][1]} | ${gameBoard[2][2]} |`);
+    output(`| ${gameBoard["7"]} | ${gameBoard["8"]} | ${gameBoard["9"]} |`);
     output("-------------");
 }
