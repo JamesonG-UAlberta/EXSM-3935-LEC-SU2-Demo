c997fac27fbd2b1bc1570c0aac4d860eb80ad6e9
James Grieve <jamesonrgrieve@gmail.com>
2023-08-14 19:28:18 -0600

    Demo;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,11 +2,19 @@ async function main() {
     // This is where the code you're actually experimenting with goes.
     
     let prompt = "Please enter your name, or 'Exit' to quit: "
+    
+    // Initialize
     let name = await input(prompt);
 
+    // Test
+    // While loops are just like decisions - you can replace the word "while" with "if" and it will function just fine.
+    // The caveat is that "while" will run again as soon as it's done (including the test).
     while (name != "Exit") 
     {
+        // Process
         output("Hello, "+name+"!");
+        // Modify
+        // You MUST modify at least one variable used in your condition to avoid infinite loops (somewhere in the loop).
         name = await input(prompt);
     }
 }
