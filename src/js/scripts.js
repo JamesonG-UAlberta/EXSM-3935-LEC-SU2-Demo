8c47db01bbcf27ff9427a7a2633cf03a57bd58f6
James Grieve <jamesonrgrieve@gmail.com>
2023-08-14 19:36:21 -0600

    While as a for;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,19 +4,27 @@ async function main() {
     let prompt = "Please enter your name, or 'Exit' to quit: "
     
     // Initialize
-    let name = await input(prompt);
+    //let name = await input(prompt);
 
     // Test
     // While loops are just like decisions - you can replace the word "while" with "if" and it will function just fine.
     // The caveat is that "while" will run again as soon as it's done (including the test).
-    while (name != "Exit") 
+    //while (name != "Exit") 
+    //{
+        
+        // Modify
+        // You MUST modify at least one variable used in your condition to avoid infinite loops (somewhere in the loop).
+        //name = await input(prompt);
+    //}
+
+
+    for (let name = await input(prompt); name != "Exit"; name = await input(prompt))
     {
         // Process
         output("Hello, "+name+"!");
-        // Modify
-        // You MUST modify at least one variable used in your condition to avoid infinite loops (somewhere in the loop).
-        name = await input(prompt);
     }
+
+    
 }
 
 
