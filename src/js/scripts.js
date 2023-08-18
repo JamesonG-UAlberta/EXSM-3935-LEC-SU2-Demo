9d1657f7e63bfd12783a5c4d41d1af8c872ddac1
James Grieve <jamesonrgrieve@gmail.com>
2023-08-17 19:36:57 -0600

    Solution;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,14 +1,30 @@
 async function main() {
-    // This is where the code you're actually experimenting with goes.
-    
-    let prompt = "Please enter your name, or 'Exit' to quit: "
-    let name = await input(prompt);
-
-    while (name != "Exit") 
+    const inventory = [
+        [1234, "Apple", 0.75], 
+        [1235, "Orange", 0.99]
+    ];
+    let total = 0;
+    let receipt = [];
+    let userInput;
+    do 
     {
-        output("Hello, "+name+"!");
-        name = await input(prompt);
-    }
+        userInput = (await input("Please enter a product code (END to exit): ")).trim().toUpperCase();
+        if (userInput != "END")
+        {
+            let found = false;
+            for (let inventoryIndex = 0; inventoryIndex < inventory.length && !found; inventoryIndex++)
+            {
+                if (inventory[inventoryIndex][0] == userInput)
+                {
+                    found = true;
+                    output(`Adding ${inventory[inventoryIndex][1]} to the receipt.`);
+                    receipt.push(inventory[inventoryIndex][1]);
+                    total += inventory[inventoryIndex][2];
+                }
+            }
+        }
+    } while (userInput != "END")
+    output(`Your total is ${total} and you purchased: ${receipt}`);
 }
 
 
